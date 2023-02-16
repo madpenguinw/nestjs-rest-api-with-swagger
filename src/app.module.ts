d01c09d7758df34e27f5b8b5035959f1b5b3c93a
Mikhail Sokolov <sokolovma@sweep-net.ru>
2023-02-17 02:46:36 +0300

    First build

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { NotesModule } from './notes/notes.module';
+import { AuthenticationModule } from './authorization/authorization.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './configuration';
+
+@Module({
+  imports: [
+    NotesModule,
+    AuthenticationModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Notes API')
+    .setDescription('The notes API description')
+    .setVersion('1.0')
+    .addApiKey(
+      {
+        type: 'apiKey',
+        name: 'X-API-KEY',
+        in: 'header',
+        description: 'Enter your API key',
+      },
+      'X-API-KEY',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+
+bootstrap();
